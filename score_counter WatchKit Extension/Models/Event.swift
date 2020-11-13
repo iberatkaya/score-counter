8da7393164a43ad1a0252199961901c045655120
iberatkaya <beratkaya1998@gmail.com>
2020-11-14 02:01:01 +0300

    Added a UUID, encoder/decoder, and helper functions to increment a player score for Events, added a playerNumber for Players

--- score_counter WatchKit Extension/Models/Event.swift
+++ score_counter WatchKit Extension/Models/Event.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
-class Event: ObservableObject, Identifiable {
+class Event: ObservableObject, Identifiable, Codable {
+    var id = UUID()
+    
+    var eventId = UUID().uuidString
     @Published var title: String
     @Published var firstPlayer: Player
     @Published var secondPlayer: Player
@@ -9,5 +12,57 @@ class Event: ObservableObject, Identifiable {
         self.title = title
         self.firstPlayer = firstPlayer
         self.secondPlayer = secondPlayer
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        title = try values.decode(String.self, forKey: .title)
+        firstPlayer = try values.decode(Player.self, forKey: .firstPlayer)
+        secondPlayer = try values.decode(Player.self, forKey: .secondPlayer)
+    }
+    
+    func incrementPlayerScore(playerNumber: UInt, amount: UInt) {
+        if(playerNumber == firstPlayer.playerNumber){
+            let (value, overflow) = firstPlayer.score.addingReportingOverflow(amount)
+            if !overflow {
+                firstPlayer.score = value
+            }
+        }
+        else if(playerNumber == secondPlayer.playerNumber) {
+            let (value, overflow) = secondPlayer.score.addingReportingOverflow(amount)
+            if !overflow {
+                secondPlayer.score = value
+            }
+        }
+    }
+    
+    func decrementPlayerScore(playerNumber: UInt, amount: UInt){
+        if(playerNumber == firstPlayer.playerNumber){
+            let (value, overflow) = firstPlayer.score.subtractingReportingOverflow(amount)
+            if !overflow {
+                firstPlayer.score = value
+            }
+        }
+        else if(playerNumber == secondPlayer.playerNumber) {
+            let (value, overflow) = secondPlayer.score.subtractingReportingOverflow(amount)
+            if !overflow {
+                secondPlayer.score = value
+            }
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case firstPlayer
+        case secondPlayer
+        case eventId
+   }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(title, forKey: .title)
+        try container.encode(firstPlayer, forKey: .firstPlayer)
+        try container.encode(secondPlayer, forKey: .secondPlayer)
+        try container.encode(eventId, forKey: .eventId)
     }
 }
--- score_counter WatchKit Extension/Models/Player.swift
+++ score_counter WatchKit Extension/Models/Player.swift
@@ -1,9 +1,12 @@
-struct Player {
+struct Player: Codable{
     var name: String
     var score: UInt
+    var playerNumber: UInt
     
-    init(name: String, score: UInt) {
+    
+    init(name: String, score: UInt, playerNumber: UInt) {
         self.name = name
         self.score = score
+        self.playerNumber = playerNumber
     }
 }
